538b46c04167e11e8d499605410869e4304416d1
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2011-04-05 17:46:23 +0200

    Thesis presentation for diploma seminar

---
+++ presentationSeminar2/freemind/why.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1301929510439" ID="ID_1650305609" LINK="structure.mm" MODIFIED="1302012813164" STYLE="bubble" TEXT="Why?">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1301929547803" MODIFIED="1302012803142" POSITION="right" TEXT="Motivation">
+<icon BUILTIN="idea"/>
+<node CREATED="1301930209739" MODIFIED="1302012803143" TEXT="Paradigm Shift =&gt; Astroinformatics"/>
+<node CREATED="1301930215699" MODIFIED="1302012803144" TEXT="Distributed processing"/>
+<node CREATED="1301930224499" MODIFIED="1302012803145" TEXT="www analogy"/>
+</node>
+</node>
+</map>
