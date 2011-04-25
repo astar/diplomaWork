4da83984a67864294d2471c29832aa3e768dab86
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2011-04-25 16:18:12 +0200

    pf now works with Ondrejov fits too usage: pf file 1 (0 == SDSS 1 == OND)

--- pf/pf.py
+++ pf/pf.py
@@ -3,22 +3,36 @@ import sys, pyfits, numpy as np, pylab as p, matplotlib.pyplot as plt
 
 #show position of some lines
 Halpha = 6564.614
+ond = 0
 
-
-if len(sys.argv) == 2:
+if len(sys.argv) > 1:
     file = sys.argv[1]
 else:
     print 'This utility plot fits file passed as argument'
+if len(sys.argv) == 3:
+    if sys.argv[2] == '1':
+        ond = 1 # Ondrejov Fit
 
+print file
+print ond
 
-def read(file):
+def read(file,type):
     """ Read fits file. Convert wavelength to angstroms """ 
-    data = pyfits.getdata(file)
-    w = lambda x : 10.0**(3.5796 + x*10.0**(-4))
-    x = np.arange(1,data[0].size + 1)
-    xx  = w(x) # convert to actual wavelenght
-    return np.asarray([xx, data[0]])
+    if type:
+        print file
+        hdu = pyfits.open(file)
+        data = hdu[1].data
+        x = data.field('WAVE')
+        y = data.field('FLUX')
+        return np.asarray([x, y])
 
+    else:
+        data = pyfits.getdata(file)
+        w = lambda x : 10.0**(3.5796 + x*10.0**(-4))
+        x = np.arange(1,data[0].size + 1)
+        xx  = w(x) # convert to actual wavelenght
+        return np.asarray([xx, data[0]])
+    
 def plot(file,xdata,ydata,spLine):
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -40,7 +54,7 @@ def plot(file,xdata,ydata,spLine):
 
     plt.show()
 
-data = read(file)
+data = read(file,ond)
 plot(file, data[0,:], data[1,:], Halpha)
 
 
